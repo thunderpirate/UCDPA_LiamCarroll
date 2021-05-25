15b45ad3fe96aeb3803fa506bc84c1e62592c1e0
Liam Carroll <liam@example.com>
2021-05-25 11:11:47 -0300

    Initial commit to Test

---
+++ main.py
@@ -0,0 +1,14 @@
+# Step 1: Importation of Python packages to be used in data gathering, analysis, visualization and machine learning
+import pandas as pd
+import requests
+import json
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+
+print('x')
